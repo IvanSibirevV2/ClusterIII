c0039d6fe51654e5265e833254eb4127ab24f3dd
IVS <IVS@IVS>
2023-08-17 10:26:43 +0300

    AutoCommit_17 августа 2023 г. 10:26:42_SibNout2020

---
+++ ClusterIII_EXE/ClusterIII/Сlustering/Сentroid.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClusterIII.Сlustering
+{
+    public class Сentroid
+    {
+        private List<List<System.Double>> LLD = new List<List<double>>();
+        private int p__N = 2;
+        public int p_N { get { return this.p__N; } set { this.p__N = value; } }
+        public Сentroid() { }
+        public Сentroid Set(Сentroid _this=null) 
+        {
+
+            return this;
+        }
+    }
+}
